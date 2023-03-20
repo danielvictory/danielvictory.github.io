c9ab99648e93d6ee66cf6cbf7eafa3d189403fae
danielvictory <danieljvictory@gmail.com>
2023-03-20 14:47:08 -0500

    display holiday data

--- js/script.js
+++ js/script.js
@@ -5,11 +5,18 @@ const userYear = document.getElementById('year-input');
 const userMonth = document.getElementById('month-input');
 const userDay = document.getElementById('day-input');
 
+const $cardContainer = $('#card-container');
+let $cardName = $('#card-name-1');
+let $cardDesc = $('#card-desc-1');
+
+let holidayCount = 0;
 let countryList = [];
 let holidayList = [];
 
 handleGetCountries();
-handleGetHolidays();
+//handleGetHolidays();
+
+$('form').on('submit', handleGetHolidays);
 
 // API call for country list
 function handleGetCountries() {
@@ -29,9 +36,10 @@ function handleGetCountries() {
 //console.log(countryList[0])
 
 // API call for holidays
-function handleGetHolidays(){
+function handleGetHolidays(evt){
+    evt.preventDefault();
     $.ajax({
-        url: `https://calendarific.com/api/v2/holidays?&api_key=${k}&country=US&year=2023&month=${userMonth.value}&day=${userDay.value}`
+        url: `https://calendarific.com/api/v2/holidays?&api_key=${k}&country=${userCountry.value}&year=${userYear.value}&month=${userMonth.value}&day=${userDay.value}`
     }).then(
         (data) => {
             //console.log(data.response.holidays)
@@ -46,5 +54,11 @@ function handleGetHolidays(){
 //console.log(holidayList)
 
 function renderHoliday(){
-    console.log(holidayList.length)
+    //console.log(holidayList.length)
+    if (holidayList.length > 0) {
+        // holidayCount++
+        // x = document.createElement('section')
+        $cardName.text(holidayList[0].name)
+        $cardDesc.text(holidayList[0].description)
+    }
 }
